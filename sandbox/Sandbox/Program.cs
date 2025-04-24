d8bd71ab9418f4197ff2283f9345345e67f0101f
T-vista <tylervistaunet@gmail.com>
2025-04-24 11:16:15 -0600

    added custom words to the read me

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // get area of a circle
+        Console.Write("Please enter the radius: ");
+        string radius_input = Console.ReadLine();
+        double radius = double.Parse(radius_input);
+
+        // compute area of circle
+        double area = Math.PI * radius * radius;
+
+        Console.WriteLine($"the area of the circle with a radius of {radius} is: {area}");
+
+        string words = "super cool";
+
+        Console.WriteLine($"Hello {words} Sandbox World!");
     }
 }
