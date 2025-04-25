07d04915d56ff4f23b25237a285a0a14e4bed455
T-vista <tylervistaunet@gmail.com>
2025-04-24 22:07:30 -0600

    finished prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        string grade_percent_text = Console.ReadLine();
+        int grade_percent = int.Parse(grade_percent_text);
+
+        // if else commands
+        string pass;
+        string grade;
+
+        if (grade_percent >= 90)
+        {
+            pass = "pass";
+            grade = "A";
+        }
+        else if (grade_percent >= 80)
+        {
+            pass = "pass";
+            grade = "B";
+        }
+        else if (grade_percent >= 70)
+        {
+            pass = "pass";
+            grade = "C";
+        }
+        else if (grade_percent >= 60)
+        {
+            pass = "fail";
+            grade = "D";
+        }
+        else
+        {
+            pass = "fail";
+            grade = "F";
+        }
+        Console.WriteLine($"Your grade is: {grade}, which means you {pass}");
     }
 }
