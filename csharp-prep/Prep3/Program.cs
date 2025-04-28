627881fd54491453a0f1fb081ac1edd93a33955b
T-vista <tylervistaunet@gmail.com>
2025-04-28 09:58:28 -0600

    added prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // get magic number
+        Random random = new Random();
+        int magic_number = random.Next(1, 101);
+
+        // declare guess
+        string guess_number_string;   
+        int guess_number;
+
+        do 
+        {
+            Console.Write("What is your guess?: ");
+            guess_number_string = Console.ReadLine();   
+            guess_number = int.Parse(guess_number_string);
+            if (guess_number < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess_number > magic_number) 
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        while (guess_number != magic_number);
+
+
     }
 }
