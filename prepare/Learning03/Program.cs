50b7c5fa12a636d0ff81fe435aaf0d30025c1c6c
T-vista <tylervistaunet@gmail.com>
2025-05-19 12:38:58 -0600

    learning 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction one = new Fraction();
+        Fraction five = new Fraction(5);
+        Fraction threequarters = new Fraction(3, 4);
+        Fraction oneThird = new Fraction(1, 3);
+
+        Console.WriteLine(one.GetFractionString());
+        Console.WriteLine(one.GetDecimalValue());
+        Console.WriteLine(five.GetFractionString());
+        Console.WriteLine(five.GetDecimalValue());
+        Console.WriteLine(threequarters.GetFractionString());
+        Console.WriteLine(threequarters.GetDecimalValue());
+        Console.WriteLine(oneThird.GetFractionString());
+        Console.WriteLine(oneThird.GetDecimalValue());
     }
 }
